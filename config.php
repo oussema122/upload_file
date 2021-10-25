d99e9bd203e76d04855b803bea15e94db4b729f2
oussema <80161665+oussema122@users.noreply.github.com>
2021-10-25 20:38:32 +0100

    Update config.php

--- config.php
+++ config.php
@@ -11,6 +11,6 @@ if (!$conn) {
     die("<script>alert('Connection Failed.')</script>");
 }
 
-$base_url = "http://localhost/File%20Upload%20&%20Download%20Using%20PHP%20&%20MySQL/source%20file/"; // Website url
+$base_url = "http://localhost/File%20Upload%20&%20Download%20Using%20PHP%20&%20MySQL/source%20file/"; 
 
 ?>
