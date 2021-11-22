17a094d8c716ea98d33ef77fd67cc043a3f8ca38
oussema <80161665+oussema122@users.noreply.github.com>
2021-11-22 19:32:34 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,77 @@
+<?php 
+
+include 'config.php';
+
+$link = "";
+$link_status = "display: none;";
+
+if (isset($_POST['upload'])) { 
+	
+	$location = "uploads/";
+	$file_new_name = date("dmy") . time() . $_FILES["file"]["name"]; 
+	$file_name = $_FILES["file"]["name"];
+	$file_temp = $_FILES["file"]["tmp_name"]; 
+	$file_size = $_FILES["file"]["size"]; 
+
+	
+
+	if ($file_size > 10485760) { 
+		echo "<script>alert('Woops! File is too big. Maximum file size allowed for upload 10 MB.')</script>";
+	} else {
+		$sql = "INSERT INTO uploaded_files (name, new_name)
+				VALUES ('$file_name', '$file_new_name')";
+		$result = mysqli_query($conn, $sql);
+		if ($result) {
+			move_uploaded_file($file_temp, $location . $file_new_name);
+			echo "<script>alert('Wow! File uploaded successfully.')</script>";
+		
+			$sql = "SELECT id FROM uploaded_files ORDER BY id DESC";
+			$result = mysqli_query($conn, $sql);
+			if ($row = mysqli_fetch_assoc($result)) {
+				$link = $base_url . "download.php?id=" . $row['id'];
+				$link_status = "display: block;";
+			}
+		} else {
+			echo "<script>alert('Woops! Something wong went.')</script>";
+		}
+	}
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" type="text/css" href="style.css">
+
+	<title>File Upload </title>
+</head>
+<body>
+	<div class="file__upload">
+		<div class="header">
+			<p><i class="fa fa-cloud-upload fa-2x"></i><span><span>up</span>load</span></p>			
+		</div>
+		<form action="" method="POST" enctype="multipart/form-data" class="body">
+			<!-- Sharable Link Code -->
+			<input type="checkbox" id="link_checkbox">
+			<input type="text" value="<?php echo $link; ?>" id="link" readonly>
+			<label for="link_checkbox" style="<?php echo $link_status; ?>">Get Sharable Link</label>
+
+			<input type="file" name="file" id="upload" required>
+			<label for="upload">
+				<i class="fa fa-file-text-o fa-3x"></i>
+				<p>
+					<strong>Drag and drop</strong> files here<br>
+					or <span>browse</span> to begin the upload
+				</p>
+			</label>
+			<button name="upload" class="btn">Upload</button>
+		</form>
+	</div>
+</body>
+</html>
